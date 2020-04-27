5234931dc062083aa8ff89997210f4531b82cc84
Still Beat <stillbeat73@gmail.com>
2020-04-27 16:31:03 +0400

    Create weather app

---
+++ WeatherTestApp/WeatherTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherTestApp
+//
+//  Created by Sind Bad on 27.04.2020.
+//  Copyright © 2020 Still Beat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
