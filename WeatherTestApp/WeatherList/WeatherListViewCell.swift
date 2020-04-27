2d99335642a72314129974a4908e27e390bac07b
Still Beat <stillbeat73@gmail.com>
2020-04-27 18:00:58 +0400

    no message

---
+++ WeatherTestApp/WeatherList/WeatherListViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherListViewCell.swift
+//  WeatherTestApp
+//
+//  Created by Sind Bad on 27.04.2020.
+//  Copyright © 2020 Still Beat. All rights reserved.
+//
+
+import UIKit
+
+class WeatherListViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
