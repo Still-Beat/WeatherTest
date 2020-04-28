9d9fe62786da82220dee07a072ef11d01af3e46d
Still Beat <stillbeat73@gmail.com>
2020-04-28 16:09:45 +0400

    no message

---
+++ WeatherTestApp/WeatherList/WeatherListVIewCellModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherListVIewCellModel.swift
+//  WeatherTestApp
+//
+//  Created by Sind Bad on 28.04.2020.
+//  Copyright © 2020 Still Beat. All rights reserved.
+//
+
+import Foundation
